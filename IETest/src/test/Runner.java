9e1db44e02b118828882611e917abb565cb483e4
INFOVITY-LT-214 <INFOVITY-LT-214@INFOVITY-LT-214>
2019-11-13 16:55:34 +0530

    commit

---
+++ IETest/src/test/Runner.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+			int a=1;
+			int c=4;
+			int b=a+c;
+			System.out.println(b);
+		
+	
+	
+		
+		
+	}
+
+}
